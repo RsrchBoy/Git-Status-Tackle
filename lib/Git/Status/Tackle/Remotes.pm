40e81a252eeb8ba9dd50750b4cb8ce3c30633a90
Chris Weyl <cweyl@alumni.drew.edu>
2012-07-20 19:05:38 -0700

    remotes, now with color!

--- lib/Git/Status/Tackle/Remotes.pm
+++ lib/Git/Status/Tackle/Remotes.pm
@@ -5,8 +5,32 @@ use warnings;
 
 use parent 'Git::Status::Tackle::Plugin';
 
+use Term::ANSIColor ':constants';
+
 sub synopsis { 'Lists our remotes' }
 
-sub list { [ `git remote -v` ] }
+sub list {
+    my ($self) = @_;
+
+    my %remotes =
+        map  { /^(\w+)\s+(\S+)/; $1 => $2 }
+        grep { /\(fetch\)/                }
+        `git remote -v`
+        ;
+
+    ### calculate how long the longest remote name is...
+    my $name_length = length 'origin'; # good a place as any to start
+    $name_length = length $_ > $name_length ? length $_ : $name_length
+        for keys %remotes;
+
+    ### and create our sprintf() template...
+    my $template = "  %-${name_length}s ";
+
+    return [
+        map { "$_\n" }
+        map { WHITE . sprintf($template, $_) . RESET . YELLOW . $remotes{$_} . RESET }
+        sort keys %remotes
+    ];
+}
 
 !!42;
